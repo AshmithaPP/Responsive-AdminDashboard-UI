84525850521e1adaaf07ed91d9eeab530399b8a6
AshmithaPP <ashmitha048@gmail.com>
2024-12-08 20:11:31 +0530

    Added complete Admin Dashboard UI with sidebar, main dashboard, and right-side components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css'
+import MainDash from './components/Sidebar/MainDash/MainDash';
+import Sidebar from './components/Sidebar/Sidebar';
+import RightSide from './components/RightSide/RightSide';
+
 
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeepscode
+      <div className='AppGlass'>
+        <Sidebar/>
+       <MainDash/>
+       <RightSide/>       
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
